efb9dda89621caec036289c700ba21a6223c7400
a9202507 <a9202507@gmail.com>
2020-10-14 13:53:50 +0800

    add "save and load" function
    add "save and load" parameter.json function.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow,QApplication#,QTableView
-from PyQt5.QtCore import QThread,pyqtSignal#,QTime
+import pythoncom
+pythoncom.CoInitialize() # to fix issue between clr and QFileDialog
+from PyQt5.QtWidgets import QMainWindow,QApplication,QFileDialog
+from PyQt5.QtCore import QThread,pyqtSignal
+
 import Ui_PyQT_IFX_VR14
 import ifx
 import time
@@ -14,6 +17,7 @@ else:
 
 import IFX_VR14_DC_LL
 import IFX_VR14_3D
+import json
 
 class myprogpressbar(QThread):
     updatebar=pyqtSignal(int)
@@ -51,7 +55,7 @@ class vr3d_thread(QThread):
     def __del__(self):
         self.wait()
     def run(self):
-        IFX_VR14_3D.vr14_3d(myWin.rail_name,myWin.vout_list_3d,myWin.start_freq,myWin.end_freq,myWin.icc_min,myWin.icc_max,myWin.freq_steps_per_decade,myWin.rise_time,myWin.cool_down_delay,myWin.start_duty,myWin.end_duty,myWin.duty_step,myWin.excel)
+        IFX_VR14_3D.vr14_3d(myWin.rail_name,myWin.vout_list_3d,myWin.start_freq,myWin.end_freq,myWin.icc_min,myWin.icc_max,myWin.freq_steps_per_decade,myWin.rise_time,myWin.cool_down_delay_3d,myWin.start_duty,myWin.end_duty,myWin.duty_step,myWin.excel)
         
     def stop(self):
         self.terminate()        
@@ -72,7 +76,7 @@ class dc_thermal_thread(QThread):
 class MyMainWindow(QMainWindow,Ui_PyQT_IFX_VR14.Ui_MainWindow):
     def __init__(self,parent=None):
         super(MyMainWindow,self).__init__(parent)
-        self.setFixedSize(800, 750)
+        self.setFixedSize(840, 940)
         self.setupUi(self)
         
         self.pushButton_8.clicked.connect(self.dc_thermal_function)
@@ -80,9 +84,16 @@ class MyMainWindow(QMainWindow,Ui_PyQT_IFX_VR14.Ui_MainWindow):
         self.pushButton_7.clicked.connect(self.stop_all_thread)
         self.pushButton.clicked.connect(self.vr3d_function)
         self.pushButton_9.clicked.connect(self.check_vendor_id)
+        self.pushButton_5.clicked.connect(self.save_parameter_function)
+        self.pushButton_6.clicked.connect(self.load_parameter_function)
+
         self.comboBox.activated.connect(self.qbox)
         self.lcdNumber.setHexMode()
         self.lcdNumber.display(0xFF)
+        self.pushButton_5.setEnabled(True)
+        self.pushButton_6.setEnabled(True)
+
+        self.tabWidget.setTabEnabled(1,False)
         
         ## init thread
         self.dc_loadline_thread=dc_thread()
@@ -96,6 +107,61 @@ class MyMainWindow(QMainWindow,Ui_PyQT_IFX_VR14.Ui_MainWindow):
         self.start_up()
     def update_bar(self,data):
         self.progressBar.setValue(data)
+        
+    def save_parameter_function(self):
+        self.update_GUI()
+        self.parameter_dict={"rail_name":self.comboBox.currentText(),
+                             "LL_vout_list":self.lineEdit_10.text(),
+                             "LL_iout_list":self.lineEdit_11.text(),
+                             "LL_cool_down_delay":self.lineEdit_14.text(),
+                             
+                             "3d_vout_list":self.lineEdit_16.text(),
+                             "3d_cool_down_delay":self.lineEdit_15.text(),
+                             "3d_start_freq":self.lineEdit.text(),
+                             "3d_end_freq":self.lineEdit_2.text(),
+                             "3d_rise_time":self.lineEdit_6.text(),
+                             "3d_fall_time":self.lineEdit_12.text(),
+                             "3d_icc_min":self.lineEdit_4.text(),
+                             "3d_icc_max":self.lineEdit_3.text(),
+                             "3d_freq_steps_per_decade":self.lineEdit_5.text(),
+                             "3d_duty_step":self.lineEdit_9.text(),
+                             "3d_start_duty":self.lineEdit_8.text(),
+                             "3d_end_duty":self.lineEdit_7.text(),
+                             
+                             }
+
+        filename_with_path=QFileDialog.getSaveFileName(self, 'Save File', '.', 'JSON Files (*.json)')
+        save_filename=filename_with_path[0]
+        if save_filename!="":
+            with open(save_filename, 'w') as fp:            
+                json.dump(self.parameter_dict, fp)
+    def load_parameter_function(self):
+        
+        dlg=QFileDialog(self, 'Open File', '.', 'JSON Files (*.json);;All Files (*)')
+        if dlg.exec_():
+            filenames=dlg.selectedFiles()
+
+            with open(filenames[0],'r') as j:
+                json_data=json.load(j)
+                print(json_data)
+            self.comboBox.setCurrentText(json_data["rail_name"])
+            self.lineEdit_10.setText(json_data["LL_vout_list"])
+            self.lineEdit_11.setText(json_data["LL_iout_list"])
+            self.lineEdit_14.setText(json_data["LL_cool_down_delay"])
+            
+            self.lineEdit_16.setText(json_data["3d_vout_list"])
+            self.lineEdit_15.setText(json_data["3d_cool_down_delay"])
+            self.lineEdit.setText(json_data["3d_start_freq"])
+            self.lineEdit_2.setText(json_data["3d_end_freq"])
+            self.lineEdit_6.setText(json_data["3d_rise_time"])
+            self.lineEdit_12.setText(json_data["3d_fall_time"])
+            self.lineEdit_4.setText(json_data["3d_icc_min"])
+            self.lineEdit_3.setText(json_data["3d_icc_max"])
+            self.lineEdit_5.setText(json_data["3d_freq_steps_per_decade"])
+            self.lineEdit_9.setText(json_data["3d_duty_step"])
+            self.lineEdit_8.setText(json_data["3d_start_duty"])
+            self.lineEdit_7.setText(json_data["3d_end_duty"])
+            
     
     def update_GUI(self):
         ## get GUI import
